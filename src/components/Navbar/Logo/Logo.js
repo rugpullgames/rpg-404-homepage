0a6503fce2274289dc614aa6a1204131b26227cf
Yuancheng Zhang <old.far.hot@gmail.com>
2022-06-12 23:58:58 +0800

    修改Navbar样式

--- src/components/Navbar/Logo/Logo.js
+++ src/components/Navbar/Logo/Logo.js
@@ -8,14 +8,9 @@ export default function Logo() {
         className='logo-rpg-icon'
         src={process.env.PUBLIC_URL + '/img/icon_rug_pull_games_logo.png'}
         alt='Twitter'
-        onClick={() => window.open('https://twitter.com/rug_pull_games', '_blank')}
+        onClick={() => window.open('https://rug-pull.games', '_blank')}
       />
-      <img
-        className='logo-rpg-404-icon'
-        src={process.env.PUBLIC_URL + '/img/icon_rpg_404.png'}
-        alt='Discord'
-        onClick={() => window.open('https://discord.com/', '_blank')}
-      />
+      <img className='logo-rpg-404-icon' src={process.env.PUBLIC_URL + '/img/icon_rpg_404.png'} alt='Discord' />
     </div>
   );
 }
