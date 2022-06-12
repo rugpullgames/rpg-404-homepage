21b196a6ebc65dc0be48fee43e5e26f70a051f86
Yuancheng Zhang <old.far.hot@gmail.com>
2022-06-12 23:56:35 +0800

    添加Logo样式

--- src/components/Navbar/Logo/Logo.js
+++ src/components/Navbar/Logo/Logo.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import './Logo.css';
 
 export default function Logo() {
-  return <div>Logo</div>;
+  return (
+    <div className='logo'>
+      <img
+        className='logo-rpg-icon'
+        src={process.env.PUBLIC_URL + '/img/icon_rug_pull_games_logo.png'}
+        alt='Twitter'
+        onClick={() => window.open('https://twitter.com/rug_pull_games', '_blank')}
+      />
+      <img
+        className='logo-rpg-404-icon'
+        src={process.env.PUBLIC_URL + '/img/icon_rpg_404.png'}
+        alt='Discord'
+        onClick={() => window.open('https://discord.com/', '_blank')}
+      />
+    </div>
+  );
 }
