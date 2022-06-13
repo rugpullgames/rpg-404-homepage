11465be64e8c43cb4fc11a3f1266db2621f764eb
Yuancheng Zhang <old.far.hot@gmail.com>
2022-06-13 22:13:04 +0800

    NFTContext里面添加soldout

--- src/components/NFTContext/NFTContext.js
+++ src/components/NFTContext/NFTContext.js
@@ -5,6 +5,7 @@ export const NFTContext = createContext({
   contractAddress: '0x5887e5C10f0dd72aA592713e7112aab5D47C5e4C',
   contractAbi: contract.abi,
   isRinkeby: true,
+  soldout: false,
   currentAccount: null,
   statusMsg: '',
   updateStatus: () => {},
