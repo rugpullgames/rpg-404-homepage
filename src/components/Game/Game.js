bfbf22fc96b34b50a087692497e407550acaed0d
Yuancheng Zhang <old.far.hot@gmail.com>
2022-06-13 12:27:14 +0800

    更新Game页面的Mint按钮图片

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -92,7 +92,7 @@ export default function Game(props) {
       <img className='game-cover' src={process.env.PUBLIC_URL + '/img/game_cover.png'} alt='Game Cover' />
       <img
         className='btn-game-mint'
-        src={process.env.PUBLIC_URL + '/img/btn_game_mint.png'}
+        src={process.env.PUBLIC_URL + '/img/btn_mint.png'}
         alt='Mint Button'
         onClick={() => props.changePage(PageName.MINT)}
       />
