2d178358883cb91a1681ca471672f4f78d4385cd
Yuancheng Zhang <old.far.hot@gmail.com>
2022-06-15 00:40:40 +0800

    拖动NFT对NFTPanel进行滑动

--- src/components/Game/NFTPanel/NFTPanel.js
+++ src/components/Game/NFTPanel/NFTPanel.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import './NFTPanel.css';
 
 export const NFT = (props) => {
+  const [posX, setPosX] = useState(null);
   const selectNft = () => {
     props.selectNft(props.metadata);
+  };
+
+  const dragStartHandler = (e) => {
+    setPosX(e.clientX);
+    e.dataTransfer.setDragImage(e.target, window.outerWidth, window.outerHeight);
+  };
+
+  const dragHandler = (e) => {
+    if (e.clientX > 0) {
+      props.scrollPanel(posX - e.clientX);
+      setPosX(e.clientX);
+    }
+    e.dataTransfer.setDragImage(e.target, window.outerWidth, window.outerHeight);
   };
 
   return (
@@ -13,40 +27,41 @@ export const NFT = (props) => {
         src={`https://ipfs.io/ipfs/${props.metadata.image.split('ipfs://')[1]}`}
         alt={props.metadata.name}
         onClick={selectNft}
+        draggable='true'
+        onDragStart={dragStartHandler}
+        onDrag={dragHandler}
       />
     </div>
   );
 };
 
 export default function NFTPanel(props) {
-  const onScroll = (e) => {
+  const container = useRef();
+  const onScrollHandler = (e) => {
     if (e.deltaY > 0) {
-      e.currentTarget.scrollLeft += 30;
+      scrollPanel(30);
     }
     if (e.deltaY < 0) {
-      e.currentTarget.scrollLeft -= 30;
+      scrollPanel(-30);
     }
+  };
+
+  const scrollPanel = (deltaX) => {
+    container.current.scrollLeft += deltaX;
   };
 
   return (
     <div className='nft-panel'>
-      <div className='nft-container' onWheel={onScroll}>
+      <div className='nft-container' ref={container} onWheel={onScrollHandler}>
         {props.metadata.map((mt) => {
-          return <NFT metadata={mt} selectNft={props.selectNft} key={`nft-metadata-${mt.dna}`} />;
+          const attr = {
+            metadata: mt,
+            selectNft: props.selectNft,
+            scrollPanel,
+          };
+          return <NFT {...attr} key={`nft-metadata-${mt.dna}`} />;
         })}
       </div>
     </div>
   );
 }
-
-// .container {
-
-// }
-// .item {
-//   padding: 10px;
-//   border: 1px solid red;
-//   display: inline-block;
-//   vertical-align:top;
-//   margin-right:20px;
-//   white-space:normal;
-// }
