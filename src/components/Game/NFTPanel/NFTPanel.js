8cba4d0e65651651b19af9f9194a082d556ddc20
Yuancheng Zhang <old.far.hot@gmail.com>
2022-07-20 18:15:20 +0800

    修复选择NFT时的拖动图片的重影的问题

--- src/components/Game/NFTPanel/NFTPanel.js
+++ src/components/Game/NFTPanel/NFTPanel.js
@@ -1,5 +1,5 @@
-import React, { useState, useRef } from 'react';
-import './NFTPanel.css';
+import React, { useState, useRef } from "react";
+import "./NFTPanel.css";
 
 export const NFT = (props) => {
   const [posX, setPosX] = useState(null);
@@ -10,7 +10,7 @@ export const NFT = (props) => {
 
   const dragStartHandler = (e) => {
     setPosX(e.clientX);
-    e.dataTransfer.setDragImage(e.target, window.outerWidth, window.outerHeight);
+    e.dataTransfer.setDragImage(e.target, 100000, 0);
   };
 
   const dragHandler = (e) => {
@@ -18,19 +18,19 @@ export const NFT = (props) => {
       props.scrollPanel(posX - e.clientX);
       setPosX(e.clientX);
     }
-    e.dataTransfer.setDragImage(e.target, window.outerWidth, window.outerHeight);
+    e.dataTransfer.setDragImage(e.target, 100000, 0);
   };
 
   return (
     <div className='nft-item' onClick={selectNft} draggable='true' onDragStart={dragStartHandler} onDrag={dragHandler}>
       <img
         className='nft-item-loading'
-        src={process.env.PUBLIC_URL + '/img/placehold_nft_loading.png'}
+        src={process.env.PUBLIC_URL + "/img/placehold_nft_loading.png"}
         alt='NFT Loading'
       />
       <img
         className='nft-image'
-        src={`https://rpg.mypinata.cloud/ipfs/${props.metadata.image.split('ipfs://')[1]}`}
+        src={`https://rpg.mypinata.cloud/ipfs/${props.metadata.image.split("ipfs://")[1]}`}
         alt={props.metadata.name}
       />
     </div>
